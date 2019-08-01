fe5cd6f3e63ad5fc17c5d3e1bf5cd052af1d7ef0
anna-delta <anerid@vmware.com>
2019-08-02 05:13:33 +0530

    Increased animation time

--- ui/src/app/modules/compare/components/compare-clouds/compare-clouds.component.ts
+++ ui/src/app/modules/compare/components/compare-clouds/compare-clouds.component.ts
@@ -11,10 +11,10 @@ import { trigger, transition, style, animate } from '@angular/animations';
     trigger('slideInOut', [
       transition(':enter', [
         style({transform: 'translateY(-100%)'}),
-        animate('200ms ease-in', style({transform: 'translateY(0%)'}))
+        animate('300ms ease-in', style({transform: 'translateY(0%)'}))
       ]),
       transition(':leave', [
-        animate('200ms ease-in', style({transform: 'translateY(-100%)'}))
+        animate('300ms ease-in', style({transform: 'translateY(-100%)'}))
       ])
     ])
   ]
